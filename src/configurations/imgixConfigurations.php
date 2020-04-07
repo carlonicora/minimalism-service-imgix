c9b8857abe4e6b372b47ff385b57771cfecaeec4
Carlo Nicora <carlo.nicora@gmail.com>
2020-04-07 10:28:15 +0100

    configurations naming convention updated

--- src/configurations/imgixConfigurations.php
+++ src/configurations/imgixConfigurations.php
@@ -22,18 +22,18 @@ class imgixConfigurations extends abstractServiceConfigurations {
      * @throws configurationException
      */
     public function __construct(){
-        if (!getenv('IMGIX_DOMAIN')){
-            throw new configurationException('imgix', 'IMGIX_DOMAIN is a required configuration');
+        if (!getenv('MINIMALISM_SERVICE_IMGIX_DOMAIN')){
+            throw new configurationException('imgix', 'MINIMALISM_SERVICE_IMGIX_DOMAIN is a required configuration');
         }
 
-        if (!getenv('IMGIX_KEY')){
-            throw new configurationException('imgix', 'IMGIX_KEY is a required configuration');
+        if (!getenv('MINIMALISM_SERVICE_IMGIX_KEY')){
+            throw new configurationException('imgix', 'MINIMALISM_SERVICE_IMGIX_KEY is a required configuration');
         }
 
-        $this->domain = getenv('IMGIX_DOMAIN');
-        $this->key = getenv('IMGIX_KEY');
+        $this->domain = getenv('MINIMALISM_SERVICE_IMGIX_DOMAIN');
+        $this->key = getenv('MINIMALISM_SERVICE_IMGIX_KEY');
 
-        $this->defaultImageHeigth = (!empty(getenv('IMGIX_DEFAULT_IMAGE_HEIGTH'))) ? (int)getenv('IMGIX_DEFAULT_IMAGE_HEIGTH') : 520;
-        $this->defaultImageWidth = (!empty(getenv('IMGIX_DEFAULT_IMAGE_WIDTH'))) ? (int)getenv('IMGIX_DEFAULT_IMAGE_WIDTH') : 520;
+        $this->defaultImageHeigth = (!empty(getenv('MINIMALISM_SERVICE_IMGIX_DEFAULT_IMAGE_HEIGTH'))) ? (int)getenv('MINIMALISM_SERVICE_IMGIX_DEFAULT_IMAGE_HEIGTH') : 520;
+        $this->defaultImageWidth = (!empty(getenv('MINIMALISM_SERVICE_IMGIX_DEFAULT_IMAGE_WIDTH'))) ? (int)getenv('MINIMALISM_SERVICE_IMGIX_DEFAULT_IMAGE_WIDTH') : 520;
     }
 }
