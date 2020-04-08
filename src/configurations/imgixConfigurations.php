d5373ed37b712b15e0c213c8833e9659c6e06295
Carlo Nicora <carlo.nicora@gmail.com>
2020-04-08 18:14:26 +0100

    configurations streamlined

--- src/configurations/imgixConfigurations.php
+++ src/configurations/imgixConfigurations.php
@@ -22,16 +22,14 @@ class imgixConfigurations extends abstractServiceConfigurations {
      * @throws configurationException
      */
     public function __construct(){
-        if (!getenv('MINIMALISM_SERVICE_IMGIX_DOMAIN')){
+        if (!($this->domain = getenv('MINIMALISM_SERVICE_IMGIX_DOMAIN'))){
             throw new configurationException('imgix', 'MINIMALISM_SERVICE_IMGIX_DOMAIN is a required configuration');
         }
 
-        if (!getenv('MINIMALISM_SERVICE_IMGIX_KEY')){
+        if (!($this->key = getenv('MINIMALISM_SERVICE_IMGIX_KEY'))){
             throw new configurationException('imgix', 'MINIMALISM_SERVICE_IMGIX_KEY is a required configuration');
         }
 
-        $this->domain = getenv('MINIMALISM_SERVICE_IMGIX_DOMAIN');
-        $this->key = getenv('MINIMALISM_SERVICE_IMGIX_KEY');
 
         $this->defaultImageHeigth = (!empty(getenv('MINIMALISM_SERVICE_IMGIX_DEFAULT_IMAGE_HEIGTH'))) ? (int)getenv('MINIMALISM_SERVICE_IMGIX_DEFAULT_IMAGE_HEIGTH') : 520;
         $this->defaultImageWidth = (!empty(getenv('MINIMALISM_SERVICE_IMGIX_DEFAULT_IMAGE_WIDTH'))) ? (int)getenv('MINIMALISM_SERVICE_IMGIX_DEFAULT_IMAGE_WIDTH') : 520;
